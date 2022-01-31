12346c14590128a8891eb0fe7fd439a8420ef721
JTZ18 <jon.taylorlim99@gmail.com>
2022-02-01 03:38:57 +0800

    ADD: About Section named Section.js

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -4,6 +4,14 @@ import styled from 'styled-components';
 function Section() {
   return (
   <Container>
+    <h1>Pixa Apes</h1>
+    <p>A collection of XXXX Pixa Apez</p>
+    <TextContainer>
+        <p>
+            XXXX Pixa Apez released into the wild. Into the wilderness, lost without any thoughts or hopes, wandering aimlessly in the world of the unknown. Found by ‘Degends’ 
+        </p>
+    </TextContainer>
+
 
   </Container>
   );
@@ -15,5 +23,32 @@ const Container = styled.div`
     width: 100vw;
     height: 100vh;
     background-color: #262b35;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+        font-size: 6rem;
+        text-decoration: underline;
+
+    }
+
+    p {
+        font-size: 3rem;
+    }
+
+`
+
+const TextContainer = styled.div`
+    display: flex;
+    padding: 3rem;
+
+    p {
+        text-align: center;
+        text-justify: inter-word;
+        letter-spacing: 0.1rem;
+        line-height: 3rem;
+        font-size: 2.5rem;
+    }
 
 `
